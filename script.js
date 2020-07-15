8169f210607fa922cb601350c17acaaace911b14
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-07-15 11:37:37 -0400

    setup

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+
+const margin = {
+    top: 100,
+    right: 20,
+    bottom: 30,
+    left: 60
+},
+    width = 920 - margin.left - margin.right
+    height = 630 - margin.top - margin.bottom
